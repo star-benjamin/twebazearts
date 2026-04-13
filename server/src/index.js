23a096aac0ec1d7116f6986fc6aadab5263862c1
star-benjamin <sukweben@gmail.com>
2026-04-13 23:53:48 +0300

    fixing index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors    = require('cors');
- 
+
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use(cors({
+  origin: [
+    'https://twebazearts.online',
+    'https://www.twebazearts.online',
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
- 
+
 app.use('/api/auth',     require('./routes/auth.routes'));
 app.use('/api/artworks', require('./routes/artwork.routes'));
 app.use('/api/services', require('./routes/service.routes'));
 app.use('/api/admin',    require('./routes/admin.routes'));
- 
+
 app.listen(process.env.PORT || 4000, () =>
   console.log(`Server on port ${process.env.PORT || 4000}`)
 );
